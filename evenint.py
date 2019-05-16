3ff6c96ff778aebaa67faaaf9be82461f69fc7b6
sangeetha19399 <50619320+sangeetha19399@users.noreply.github.com>
2019-05-16 14:03:56 +0530

    Rename even.py to evenint.py

---
+++ evenint.py
@@ -0,0 +1,8 @@
+start,end=input().split()
+start=int(start)
+end=int(end)
+for counter in range(start,end+1):
+    if(counter==start):
+        continue
+    elif(counter%2==0):
+        print(counter,end ="  ")
