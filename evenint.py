0814dd1309efb61b158e3e1fe12b2262dce9290b
sangeetha19399 <50619320+sangeetha19399@users.noreply.github.com>
2019-05-16 14:09:07 +0530

    Update evenint.py

--- evenint.py
+++ evenint.py
@@ -4,5 +4,7 @@ end=int(end)
 for counter in range(start,end+1):
     if(counter==start):
         continue
+    elif(counter==end):
+        break
     elif(counter%2==0):
-        print(counter,end ="  ")
+        print(counter,end =" ")
