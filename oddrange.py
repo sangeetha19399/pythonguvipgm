19ac0925b826d1bafdbd9a34e6d72a76c74c09b4
sangeetha19399 <50619320+sangeetha19399@users.noreply.github.com>
2019-05-16 13:59:16 +0530

    Update oddrange.py

--- oddrange.py
+++ oddrange.py
@@ -2,5 +2,7 @@ start,end=input().split()
 start=int(start)
 end=int(end)
 for counter in range(start,end+1):
-    if(counter%2!=0):
-        print(counter,end ='')
+    if(counter==1):
+        continue
+    elif(counter%2!=0):
+        print(counter,end ="  ")
