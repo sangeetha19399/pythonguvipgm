f7f99b23738df3bfc0688d120c3f0b7e92d1e63e
sangeetha19399 <50619320+sangeetha19399@users.noreply.github.com>
2019-06-24 14:27:14 +0530

    Create pro1.py

---
+++ pro1.py
@@ -0,0 +1,12 @@
+num1=int(input())
+a=[]
+for k in range(0,num1):  
+    d=input()
+    a.append(d)
+list=[]
+for k in zip(*a):
+    if (k.count(k[0])==len(k)): 
+        list.append(k[0])
+    else:
+        break
+print(''.join(list))
