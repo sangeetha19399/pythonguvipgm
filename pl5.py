db34193ca0c0f1727721b4bbbbd034cb6fb5a312
sangeetha19399 <50619320+sangeetha19399@users.noreply.github.com>
2019-07-19 10:49:58 +0530

    Create pl5.py

---
+++ pl5.py
@@ -0,0 +1,7 @@
+a2=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+b2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+c2=str(input())
+for d1 in range(20):
+  if(c2==a2[d1]):
+    print(b2[d1])
+    break
